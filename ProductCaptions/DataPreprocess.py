5a7d59ff3d0c3b2d05383ff58b2093e9158d8340
Gayathri Namasivayam <gayatrina@DESKTOP-EHAKENU.localdomain>
2019-10-18 14:06:53 -0700

    added datapreprocess

--- ProductCaptions/DataPreprocess.py
+++ ProductCaptions/DataPreprocess.py
@@ -1,10 +1,17 @@
 """
-This is the main data preprocessing file for the text/captions in the dataset
-The data that is extracted by the webscrapper is extremely noisy and significant prepreocessing needs to be Done
+This program does the main datapreprocessing for the FurnitureImageGeneration.csv file stored in the Data folder
+It takes the captions for each of the images and preprocess the text in the captions and generates a new set of
+captions and writes it to a new column in the file in the Data folder as "FurnitureEditedCaptions.csv"
+Some of the preprocessing include removing product names and product company names elimininating product identification
+numbers and size of the couches
 
+Input: filepath -- folder to the FurnitureImageGeneration.csv file
+       filename --name of the image caption pairs .csv file
 
+Output: "FurnitureEditedCaptions.csv" a file with preprocessed captions
 
 """
+
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
@@ -15,148 +22,22 @@ import re
 class DataPreprocessing:
 
     def __init__(self, filepath, filename):
-        #some of the popular brands on amazon selling sofas
-        brands= ["24 7 Shop at Home",
-        "Acme Furniture",
-        "America Luxury Sofa",
-        "American Eagle Furniture",
-        "Apt2B",
-        "Armen Living",
-        "Baxton Studio",
-        "Benjara",
-        "Benzara",
-        "Blackjack Furniture",
-        "Boca Rattan",
-        "BOWERY HILL",
-        "Brika Home",
-        "Chelsea Home",
-        "Classic Brands"
-        "Christopher Knight Home",
-        "Coaster Home Furnishings",
-        "Container Furniture Direct",
-        "DIVANO ROMA FURNITURE",
-        "DHP Emily",
-        "Dreamseat",
-        "Epic Furnishings",
-        "Ethan Allen",
-        "Flash Furniture",
-        "FDW",
-        "Furniture of America",
-        "Global Furniture USA",
-        "Glory Furniture",
-        "Great Deal Furniture",
-        "Homelegance",
-        "HOMES: Inside Out",
-        "Iconic Home",
-        "Istikbal",
-        "J and M Furniture",
-        "Jennifer Taylor Home",
-        "Kardiel",
-        "Lesro",
-        "Limari Home",
-        "Meridian Furniture",
-        "Modway",
-        "Offex",
-        "DG Casa",
-        "Oadeer Home",
-        "Pemberly Row",
-        "Poundex",
-        "Rivet",
-        "Rivet Revolve"
-        "Serta",
-        "Signature Design by Ashley",
-        "Simmons Upholstery",
-        "SOUTH CONE HOME",
-        "Stone and Beam",
-        "Sunset Trading",
-        "TK Classics",
-        "TrueModern",
-        "US Pride Furniture",
-        "VVR Homes",
-        "Zinus Ricardo",
-        "Zinus Jackie",
-        "Flash Furniture Benchcraft Maier",
-        "Ashley Furniture Signature Design",
-        "Serta RTA Palisades Collection",
-        "Homelegance Resonance",
-        "Best Choice Products",
-        "Serta Rane Collection",
-        "Zinus Lauren",
-        "Madison Home",
-        "TLY Cotton Karlstad",
-        "AODAILIH",
-        "Yaheetech",
-        "Ambesonne",
-        "Atlantic Furniture",
-        "BalsaCircle",
-        "Baxton Studio",
-        "Best Choice Products",
-        "CaliTime",
-        "ChezMax",
-        "Christopher Knight Home",
-        "CHUN YI",
-        "Coaster Home Furnishings",
-        "Convenience Concepts",
-        "Deconovo",
-        "Delta Children",
-        "East West Furniture",
-        "Efavormart.com",
-        "Emvency",
-        "Flash Furniture",
-        "Giantex",
-        "GULTMEE",
-        "H.VERSAILTEX",
-        "HGOD DESIGNS",
-        "Homelegance",
-        "HomePop",
-        "HON",
-        "Klear Vu",
-        "Lunarable",
-        "mDesign",
-        "mds",
-        "Meijiafei",
-        "Modway",
-        "Momeni Rugs",
-        "Moslion",
-        "Mugod",
-        "NATUS WEAVER",
-         "NIDITW",
-        "nuLOOM",
-        "Office Star",
-        "OFM",
-        "Pillow Perfect",
-        "POLY & BARK",
-        "Rivet",
-        "Roundhill Furniture",
-        "Safavieh",
-        "Signature Design by Ashley"
-        "Stone & Beam",
-        "Subrtex",
-        "SURE FIT",
-        "Sure Fit",
-        "Surefit",
-        "TreeWool",
-        "uxcell"
-        ]
-        brands=[w.lower()  for w in brands ]
+        text_file = open(filepath+"brands.txt", "r")
+        self.brands = text_file.read().split('\n')
+        self.brands=[w.lower()  for w in self.brands ]
 
-        #os.pardir ="C:\\Users\\Gayathri\\Documents\\Insight\\ImageCaption\\capturetocaption\\data\\raw\\sofas\\"
-        os.pardir = filepath
+        os.pardir =filepath
         self.inputdf = pd.DataFrame() #dataframe with raw data
         self.outputdf=pd.DataFrame(columns=['filename','caption']) #dataframe with preprocessed captions and filename pairs
-
-        self.len_traindir = 8000 #currently set to 8000
-
-        self.load_file(filename)
-        #self.makedir()
-
+        self.load_file()
 
     #load the raw data into the inputdf dataframe
-    def load_file(self, filename):
-        #os.filename= os.pardir+"FurnitureImageGeneration_noduplicates.csv"
-        os.filename= os.pardir+filename
+    def load_file(self):
+        os.filename= os.pardir+"FurnitureImageGeneration.csv"
         self.inputdf = pd.read_csv(os.filename)
         self.inputdf=self.inputdf.drop(columns=["Unnamed: 0"])
+        self.inputdf.reset_index(drop=True, inplace=True)
+        self.len_traindir = self.inputdf.shape[0]-1 #currently set to 8000
 
     #dispaly the an image and its caption using an index from the inputdf
     def display_image_caption(self, index):
@@ -168,28 +49,6 @@ class DataPreprocessing:
 
     def __input_dataframe_size(self):
         return (self.inputdf.shape[0])
-
-    #Create the train and test directories
-    #can be improved to shuffle and store random data
-    def maketrain_test_dir(self):
-        train_dataset_dir= os.pardir+"train1\\"
-        test_dataset_dir= os.pardir+"test1\\"
-        os.mkdir(train_dataset_dir)
-        os.mkdir(test_dataset_dir)
-        print(self.inputdf.shape[0])
-        #len_trainset= abs(self.__input_dataframe_size()*0.8)
-        for i, filename in enumerate(self.inputdf[0:self.len_traindir]['filename']):
-            print(filename)
-            src=os.pardir+filename
-            dst=train_dataset_dir+filename
-            shutil.copyfile(src, dst)
-        for i, filename in enumerate(df[self.len_traindir:]['filename']):
-            print(filename)
-            src=os.pardir+filename
-            dst=test_dataset_dir+filename
-            shutil.copyfile(src, dst)
-        #df_train_captions=df['caption'][0:len_trainset]
-        #df_test_captions=df['caption'][len_trainset:]
 
     # internal funtion to take a word and transform it
     def __preprocess_word(self, word):
@@ -216,13 +75,8 @@ class DataPreprocessing:
     def __preprocess_str(self, caption):
         caption=re.sub(r'w\/', "with", caption) #replace w/ with with
         caption=re.sub(r'\&', "and", caption) #replace & with and
-        caption=re.sub(r'[,|(|)|\/]', " ", caption) #eliminate breackets
+        caption=re.sub(r'[,|(|)|\/]', " ", caption) #eliminate brackets
         caption=" ".join([self.__preprocess_word(word) for word in caption.split() ]) #preprocess each word in the caption and combine them to a list
-        #caption=" ".join([word for word in caption.split() if word.isalpha() ])
-        #caption=re.sub(r'\s+[A-z|a-z]+(\-|\d+)[A-z|a-z|0-9|-]*', " ", caption)
-        #caption=re.sub(r'[A-Z|a-z]+(-)*[A-Z|a-z]*\d+\w*', "", caption)
-        #caption=re.sub(r'[/|\-|\|]', " ", caption)
-        #caption=re.sub(r'\s*\d+(\.\d+)*\s*"*\s*(x|X)\d+(\.\d+)*\s*"*\s*(x|X)\d+(\.\d+)*\s*"*\s*', " ", caption)
         caption=re.sub(r'\s+(Inch|in|W|H|D)\s+', " ", caption) #eliminate Inch and other dimension indicators
         caption=re.sub(r'\s+x\s+', " ", caption)
         return caption.lower()
@@ -231,7 +85,6 @@ class DataPreprocessing:
     def preprocess_caption(self):
         for i in range(0,self.len_traindir):
             self.outputdf= self.outputdf.append({'filename':self.inputdf['filename'][i], 'caption':self.__preprocess_str(self.inputdf['caption'][i])}, ignore_index=True)
-        #print(self.outputdf)
         self.remove_furniturecompanies_fromcaption()
 
     #returns dictonary of all the words in the preprocessed captions data frame
@@ -285,7 +138,7 @@ class DataPreprocessing:
 
         #combine the furniture names with the existing list of brands and
         #delete those from the dictoinary
-        set_captlist=set(captlist+brands)
+        set_captlist=set(captlist+self.brands)
         print(set_captlist)
         newcol=[]
         for i in range(0,self.outputdf.shape[0]):
@@ -300,7 +153,7 @@ class DataPreprocessing:
                 newcol.append(self.outputdf['caption'][i])
         self.outputdf['caption_old']=self.inputdf['caption']
         self.outputdf['caption_new']=newcol
-        self.outputdf.to_csv(os.pardir+"Amazon_furniture_editedcaptions_noduplicates.csv")
+        self.outputdf.to_csv(os.pardir+"FurnitureEditedCaptions.csv")
 
     #compute the vocabulary length
     def compute_the_vocabulary():
@@ -311,12 +164,3 @@ class DataPreprocessing:
             for word in ls:
                     vocabulary.add(word)
         return len(vocabulary)
-
-def main():
-        DataPreprocess = DataPreprocessing()
-        #DataPreprocess.display_image_caption(2)
-        DataPreprocess.preprocess_caption()
-
-
-if __name__ == "__main__":
-        main()
