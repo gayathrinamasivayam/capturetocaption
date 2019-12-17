91f130bfdc59637521a1eb611e61c69418470c87
Gayathri <gayathri.namasivayam1@gmail.com>
2019-12-16 22:44:10 -0800

    added purepaths and new dirs

--- ProductCaptions/src/inference.py
+++ ProductCaptions/src/inference.py
@@ -1,15 +1,11 @@
-
 from keras.models import load_model
-
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
-
 from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing.image import img_to_array
-
 from keras.models import Model
 from keras.layers import Input
 import pandas as pd
@@ -19,12 +15,16 @@ import numpy as np
 import cv2
 from numpy import argmax
 from pathlib import Path
+
 #import pydot
 #from keras.utils import plot_model
+
 import timeit
 import time
 import argparse
+import errno
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 class load_test:
     """
     This program can be used to obtain a description of any image containing a sofa
@@ -37,11 +37,13 @@ class load_test:
     """
     def __init__(self, filepath, imagetestfile):
         os.pardir = Path(filepath)
-        dir_pretrained = Path(os.getcwd()).parents[0]/"Data"/"Images"
-
-        self.model = load_model(dir_pretrained/'ImageCaption_ftrain_0.8fvalid_0.8rstate_4441agdata_3000batch_500model_200.h5')
-        with open(dir_pretrained/'tokenizer_sofas_ftrain_0.8fvalid_0.8rstate_4441agdata_3000batch_500model_200.pickle', 'rb') as handle:
+        dir_pretrained = Path(os.getcwd()).parents[0]/"data"/"models"
+        try:
+            self.model = load_model(dir_pretrained/'ImageCaption_ftrain_0.8fvalid_0.8rstate_4441agdata_3000batch_500model_200.h5')
+            with open(dir_pretrained/'tokenizer_sofas_ftrain_0.8fvalid_0.8rstate_4441agdata_3000batch_500model_200.pickle', 'rb') as handle:
                 self.tokenizer = pickle.load(handle)
+        except (FileNotFoundError, OSError):
+            print("Error: {} Check if the pretrained model and the tokenizer exist in {}".format(errno.ENOENT, str(dir_pretrained)))
 
         self.testfilename=imagetestfile
         self.maxlen = 30 #maxlength of description
@@ -110,7 +112,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--filepath", type=str,
-                        default = Path(os.getcwd()).parents[0]/"Data"/"Images",
+                        default = Path(os.getcwd()).parents[0]/"data"/"images",
                         help = "Path to the folder containing the image file that you want a caption for")
     parser.add_argument("--filename", type=str,
                         default = "amazonfurniture_page1_9.png",
@@ -118,4 +120,3 @@ if __name__ == "__main__":
 
     FLAGS = parser.parse_args()
     test=load_test(FLAGS.filepath, FLAGS.filename)
-#test=load_test("C:\\Users\\Gayathri\\Documents\\Insight\\ImageCaption\\capturetocaption\\data\\raw\\Testing\\src\\Data\\Images\\", "amazonfurniture_page1_2.png")
